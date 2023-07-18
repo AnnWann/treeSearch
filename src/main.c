a588e7770b70e952eb2f376dbc8e04351e47833e
Uaná Gasparim <uanabgasparim@gmail.com>
2023-07-18 07:02:20 -0300

    implemented the path openning logic

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include "../lib/parser.h"
 #include "../lib/treeOfFiles.h"
@@ -7,23 +9,34 @@
 
 int main(void)
 {
-    char* root = NULL, path = NULL, searchResult = NULL;
+    char* root = NULL, *path = NULL, *searchResult = NULL, *command;
     uint8_t answer = 0;
     if(hasFileTree()){
-        FILE *fileTree = getFileTree();
-        root = getRoot(fileTree);
         appLoop:
+        FILE *fileTree = getFileTree();
+        
+        if(root == NULL || strlen(root) == 0) getRoot(fileTree, &root);
+
         while(1){
             printf("root: %s\nDo you want to search on this root?\n1: Yes\n2: No\n0: Quit\n", root);
             scanf("%hhu", &answer);
+
             switch(answer){
                 case 1:
                     printf("Enter the file/directory you want to find: ");
+                    path = malloc(256 * sizeof(char));
                     scanf("%255s", path);
-                    searchResult = search(path,  fileTree);
-                    if(searchResult == NULL || searchResult == ""){
+                    search(&path,  fileTree, &searchResult);
+                    free(path);
+                    if(searchResult == NULL || strlen(searchResult) == 0){
                         printf("There is no such path");
-                    }else openPath(searchResult);
+                    }else{
+                        command = malloc(sizeof(searchResult));
+                        sprintf(command, "start \"%s\"", searchResult);
+                        system(command);
+                        free(command);
+                        free(searchResult);
+                    }
                     break;
                 case 2: 
                     goto enterRoot;
@@ -38,7 +51,7 @@ int main(void)
         enterRoot:
         printf("Please enter the root of your search: ");
         scanf("%255s", root);
-        createFileTree();
+        createFileTree(root);
         goto appLoop;
     }
 
